c736e539f0d430537f795bf5003106008686877c
Kevin Dev <kevin.developer71@gmail.com>
2024-02-12 23:19:28 -0600

    refactor: add key to suspense

--- src/app/(pokemon)/type/page.tsx
+++ src/app/(pokemon)/type/page.tsx
@@ -3,6 +3,7 @@ import type PokeAPI from "pokedex-promise-v2"
 
 import PokemonListEmpty from "@/modules/pokemon/_components/empty-states/pokemon-list-empty"
 import PokemonListItem from "@/modules/pokemon/_components/pokemon-list-item"
+import PokemonListItemSkeleton from "@/modules/pokemon/_components/skeletons/pokemon-list-item-skeleton"
 import PokemonListSkeleton from "@/modules/pokemon/_components/skeletons/pokemon-list-skeleton"
 import { filterAndSortPokemons } from "@/modules/pokemon/utils/filter-and-sort-pokemons"
 import { fetchResource } from "@/server/pokeapi/actions/fetch-resource"
@@ -21,20 +22,9 @@ const PokemonTypePage = async ({
   // SERVICES
   const result = await fetchResource<PokeAPI.Type>(String(url))
 
-  // SERVICE
-  const pokemonsWithDataArr: PokeAPI.Pokemon[] = []
-
-  // const pokemonDataArr = await fetchResource<PokeAPI.Pokemon[]>(results.map((result) => result.url)) // Too slow
-  // FETCH POKEMON DETAILS in Parallel
-  await Promise.allSettled(
-    result.pokemon.map((_result) => {
-      return fetchResource<PokeAPI.Pokemon>(_result.pokemon.url)
-    })
-  ).then((promiseResults) => {
-    promiseResults.forEach((promiseResult) => {
-      if (promiseResult.status === "fulfilled") pokemonsWithDataArr.push(promiseResult.value)
-    })
-  })
+  const pokemonsWithDataArr = await fetchResource<PokeAPI.Pokemon[]>(
+    result.pokemon.map((_result) => _result.pokemon.url)
+  )
 
   // APPLY FILTERS
   const filteredPokemonsWithDataArr = filterAndSortPokemons({
@@ -45,13 +35,17 @@ const PokemonTypePage = async ({
   const sortBy = searchParams.sortBy ?? ""
 
   return (
-    <Suspense fallback={<PokemonListSkeleton />}>
+    <Suspense key={String(url) + searchParams.name} fallback={<PokemonListSkeleton />}>
       <section className="mx-auto grid w-full grid-flow-dense grid-cols-[repeat(auto-fit,minmax(180px,1fr))] gap-3 rounded-xl bg-white px-2 pb-3 pt-3">
         {/* EMPTY STATE */}
         {filteredPokemonsWithDataArr.length === 0 && <PokemonListEmpty />}
         {/* LIST ALL */}
         {filteredPokemonsWithDataArr.map((pokemon) => {
-          return <PokemonListItem key={pokemon.id} pokemon={pokemon} sortBy={String(sortBy)} />
+          return (
+            <Suspense key={pokemon.id} fallback={<PokemonListItemSkeleton name={pokemon.name} />}>
+              <PokemonListItem key={pokemon.id} pokemon={pokemon} sortBy={String(sortBy)} />
+            </Suspense>
+          )
         })}
       </section>
     </Suspense>
